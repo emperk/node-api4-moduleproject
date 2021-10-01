4cdbb597331e4ff43001b93521aaedf2d55b09fd
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-30 21:07:16 -0700

    work

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 8000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req, res) => {
+  res.json({
+    message: 'its working!'
+  })
+})
+
+server.use('*', (req, res) => {
+  res.send(`<h1>Hey</h1>`)
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`*** listening on port: ${PORT} ***`)
+})
