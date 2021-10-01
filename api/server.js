792b26c2ee190b28e80a07d2be2ed3a5352aa3b1
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-30 21:36:19 -0700

    s

--- api/server.js
+++ api/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const usersRouter = require('./users/users-router');
 const cors = require('cors');
 const server = express();
+const usersRouter = require('./users/users-router');
 
 server.use(express.json());
 server.use(cors());
-
 server.use('/api', usersRouter)
 
 // server.use('*', (req, res) => {
 //   res.send(`<h1>Work in Progress</h1>`)
 // })
+
+server.get('/', (req, res) => {
+  res.send(`<h1>Work in Progress!</h1>`)
+})
 
 module.exports = server;
